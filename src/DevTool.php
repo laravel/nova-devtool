2d6f7dc61981281bddcd8aa0071485ad628a3c91
Mior Muhammad Zaki <crynobone@gmail.com>
2024-08-02 23:32:43 +0800

    wip
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- src/DevTool.php
+++ src/DevTool.php
@@ -4,12 +4,13 @@ namespace Laravel\Nova\DevTool;
 
 use Illuminate\Support\Str;
 use Laravel\Nova\Actions\ActionResource;
+use Laravel\Nova\Nova;
 use Laravel\Nova\Resource;
 use Orchestra\Workbench\Workbench;
 use ReflectionClass;
 use Symfony\Component\Finder\Finder;
 
-class Nova extends \Laravel\Nova\Nova
+class DevTool extends Nova
 {
     /**
      * Register all of the resource classes in the given directory.
