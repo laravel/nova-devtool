b00d193d488f6b2cac5a536224412c9b9f9d02ed
Mior Muhammad Zaki <crynobone@gmail.com>
2024-08-08 16:33:17 +0800

    wip
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- src/DevToolServiceProvider.php
+++ src/DevToolServiceProvider.php
@@ -2,7 +2,6 @@
 
 namespace Laravel\Nova\DevTool;
 
-use Composer\InstalledVersions;
 use Illuminate\Contracts\Events\Dispatcher as EventDispatcher;
 use Illuminate\Support\ServiceProvider;
 use Laravel\Nova\Console\ActionCommand;
@@ -135,7 +134,6 @@ class DevToolServiceProvider extends ServiceProvider
         if (
             $this->app->runningInConsole()
             && \defined('TESTBENCH_WORKING_PATH')
-            && InstalledVersions::isInstalled('orchestra/workbench')
         ) {
             tap($this->app->make('events'), function (EventDispatcher $event) {
                 $event->listen(InstallStarted::class, [Listeners\InstallingWorkbench::class, 'handle']);
