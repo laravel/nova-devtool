e15377dacd0ad6410b0cb1fbf2abb8c0a5bbdc42
Mior Muhammad Zaki <crynobone@gmail.com>
2024-08-29 19:48:56 +0800

    wip
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

---
+++ types/dist/components.d.ts
@@ -0,0 +1,9 @@
+/**
+ * @typedef {import('vue').App} VueApp
+ */
+/**
+ * @param {VueApp} app
+ */
+export function registerViews(app: VueApp): void;
+export type VueApp = import("vue").App;
+//# sourceMappingURL=components.d.ts.map
---
+++ types/dist/fields.d.ts
@@ -0,0 +1,6 @@
+/**
+ * @param {VueApp} app
+ */
+export function registerFields(app: VueApp): void;
+export type VueApp = import("vue").App;
+//# sourceMappingURL=fields.d.ts.map
