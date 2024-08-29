ff73c5db375d733ba23d47c9b07e76ab1738da8b
Mior Muhammad Zaki <crynobone@gmail.com>
2024-08-30 01:19:46 +0800

    wip
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- types/@types/laravel-nova/index.d.ts
+++ types/@types/laravel-nova/index.d.ts
@@ -1,13 +1,119 @@
 declare module 'laravel-nova' {
+  export type AxiosResponse = import("axios").AxiosResponse;
   import type { Errors as FormErrors } from 'laravel-nova-devtool'
 
-  export namespace PreventsFormAbandonment {}
-  export namespace PreventsModalAbandonment {}
-  export namespace DependentFormField {}
-  export namespace HandlesFormRequest {}
-  export namespace HandlesUploads {}
-  export namespace Localization {}
-  export namespace MetricBehavior {}
+  export class Errors extends FormErrors {}
+
+  export namespace PreventsFormAbandonment {
+    function data(): {
+      removeOnNavigationChangesEvent: any;
+      removeOnBeforeUnloadEvent: any;
+      navigateBackUsingHistory: boolean;
+    };
+    namespace methods {
+      function updateFormStatus(): void;
+      function enableNavigateBackUsingHistory(): void;
+      function disableNavigateBackUsingHistory(): void;
+      function handlePreventFormAbandonment(proceed: Function, revert: Function): void;
+      function handlePreventFormAbandonmentOnInertia(event: Event): void;
+      function handlePreventFormAbandonmentOnPopState(event: any): void;
+      function handleProceedingToPreviousPage(reset?: boolean): void;
+      function handleProceedingToNextPage(): void;
+      function proceedToPreviousPage(url: string): void;
+      let allowLeavingForm: import("vuex").MutationMethod;
+      let preventLeavingForm: import("vuex").MutationMethod;
+      let triggerPushState: import("vuex").MutationMethod;
+      let resetPushState: import("vuex").MutationMethod;
+    }
+    namespace computed {
+      let canLeaveForm: import("vuex").Computed;
+      let canLeaveFormToPreviousPage: import("vuex").Computed;
+    }
+  }
+  export namespace PreventsModalAbandonment {
+    namespace methods {
+      function updateModalStatus(): void;
+      function handlePreventModalAbandonment(proceed: Function, revert: Function): void;
+      let allowLeavingModal: import("vuex").MutationMethod;
+      let preventLeavingModal: import("vuex").MutationMethod;
+    }
+    namespace computed {
+      let canLeaveModal: import("vuex").Computed;
+    }
+  }
+  export namespace DependentFormField {
+    export function data(): {
+      dependentFieldDebouncer: any;
+      canceller: any;
+      watchedFields: {};
+      watchedEvents: {};
+      syncedField: any;
+      pivot: boolean;
+      editMode: string;
+    };
+    export namespace methods {
+      function setInitialValue(): void;
+      function fillIfVisible(formData: FormData, attribute: string, value: any): void;
+      function syncField(): void;
+      function onSyncedField(): void;
+      function emitOnSyncedFieldValueChange(): void;
+      function syncedFieldValueHasNotChanged(): boolean;
+    }
+    export namespace computed {
+      function currentField(): object;
+      function currentlyIsVisible(): boolean;
+      function currentlyIsReadonly(): boolean;
+      function dependsOn(): string[];
+      function currentFieldValues(): {
+          [key: string]: any;
+      };
+      function dependentFieldValues(): {
+          [key: string]: any;
+      };
+      function encodedDependentFieldValues(): string;
+      function syncFieldEndpoint(): string;
+    }
+  }
+  export namespace HandlesFormRequest {
+    function data(): {
+      validationErrors: Errors;
+    };
+    namespace methods {
+      function handleResponseError(error: AxiosResponse): void;
+      function handleOnCreateResponseError(error: AxiosResponse): void;
+      function handleOnUpdateResponseError(error: AxiosResponse): void;
+      function resetErrors(): void;
+    }
+  }
+  export namespace HandlesUploads {
+    function data(): {
+      isWorking: boolean;
+      fileUploadsCount: number;
+    };
+    namespace methods {
+      function handleFileUploadFinished(): void;
+      function handleFileUploadStarted(): void;
+    }
+  }
+  export namespace Localization {
+    namespace methods {
+      function __(key: string, replace: {
+        [key: string]: value;
+      }): string;
+    }
+  }
+  export namespace MetricBehavior {
+    namespace methods {
+      function fetch(): void;
+      function handleFetchCallback(): () => void;
+    }
+    namespace computed {
+      function metricEndpoint(): string;
+      function metricPayload(): {
+          [key: string]: value;
+      };
+    }
+  }
   export namespace FieldValue {
     namespace methods {
       function isEqualsToValue(value: any): boolean;
@@ -114,7 +220,6 @@ declare module 'laravel-nova' {
       function copyValueToClipboard(value: string): void;
     }
   }
-  export class Errors extends FormErrors {}
 
   export function mapProps(attributes: Array<string>): {[key: string]: any};
   export function useLocalization(): {__: (key: string, replace: {[key: string]:string}) => string};
