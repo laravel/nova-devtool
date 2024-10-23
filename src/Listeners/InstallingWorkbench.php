11dfd3d67a71544773b7c3d01dd289ecd7fcbb33
Mior Muhammad Zaki <crynobone@gmail.com>
2024-10-23 14:41:13 +0800

    wip
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- src/Listeners/InstallingWorkbench.php
+++ src/Listeners/InstallingWorkbench.php
@@ -5,10 +5,7 @@ namespace Laravel\Nova\DevTool\Listeners;
 use Illuminate\Contracts\Console\Kernel as ConsoleKernel;
 use Illuminate\Filesystem\Filesystem;
 use Orchestra\Workbench\Events\InstallStarted;
-use Orchestra\Workbench\Workbench;
 use RuntimeException;
-
-use function Orchestra\Testbench\join_paths;
 
 class InstallingWorkbench
 {
@@ -32,9 +29,6 @@ class InstallingWorkbench
         if ($event->input->hasOption('basic') && $event->input->getOption('basic') === true) {
             throw new RuntimeException('Nova Devtool does not support installation with --basic` option');
         }
-
-        Workbench::swapFile('config', join_paths(__DIR__, '..', '..', 'stubs', 'testbench.stub'));
-        Workbench::swapFile('seeders.database', join_paths(__DIR__, '..', '..', 'stubs', 'DatabaseSeeder.stub'));
 
         $this->kernel->call('make:user-model');
         $this->kernel->call('make:user-factory');
