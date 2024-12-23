d5cdbe9910d33421109db523c2c1e5d9367b26b5
Mior Muhammad Zaki <crynobone@gmail.com>
2024-12-24 07:04:41 +0800

    fix: correct generated namespace (#8)
    * wip
    
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>
    
    * wip
    
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>
    
    * wip
    
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>
    
    * wip
    
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>
    
    * wip
    
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>
    
    ---------
    
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

---
+++ tests/Command/DevToolCommandTest.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace Tests\Command;
+
+use PHPUnit\Framework\Attributes\DataProvider;
+use PHPUnit\Framework\Attributes\Test;
+
+class DevToolCommandTest extends CommandTestCase
+{
+    #[Test]
+    #[DataProvider('environmentFileDataProviders')]
+    public function it_can_run_devtool_command_with_installation(?string $answer, bool $createEnvironmentFile)
+    {
+        $this->artisan('workbench:devtool', ['--install' => true])
+            ->expectsConfirmation('Prefix with `Workbench` namespace?', answer: 'yes')
+            ->expectsChoice("Export '.env' file as?", $answer, [
+                'Skip exporting .env',
+                '.env',
+                '.env.example',
+                '.env.dist',
+            ])
+            ->expectsConfirmation('Generate `workbench/bootstrap/app.php` file?', answer: 'no')
+            ->expectsConfirmation('Generate `workbench/bootstrap/providers.php` file?', answer: 'no')
+            ->assertSuccessful();
+
+        $this->assertCommandExecutedWithDevTool();
+        $this->assertCommandExecutedWithInstall();
+        $this->assertFromEnvironmentFileDataProviders($answer, $createEnvironmentFile);
+    }
+
+    #[Test]
+    #[DataProvider('environmentFileDataProviders')]
+    public function it_can_run_devtool_command_with_basic_installation(?string $answer, bool $createEnvironmentFile)
+    {
+        $this->assertCommandFailedWithBasicInstall();
+
+        $this->artisan('workbench:devtool', ['--install' => true, '--basic' => true])
+            ->assertSuccessful();
+    }
+
+    #[Test]
+    public function it_can_run_devtool_command_without_installation()
+    {
+        $this->artisan('workbench:devtool', ['--no-install' => true])
+            ->expectsConfirmation('Prefix with `Workbench` namespace?', answer: 'yes')
+            ->assertSuccessful();
+
+        $this->assertCommandExecutedWithDevTool();
+        $this->assertCommandExecutedWithoutInstall();
+    }
+
+    #[Test]
+    public function it_can_be_installed_with_no_interaction_options()
+    {
+        $this->artisan('workbench:devtool', ['--no-install' => true, '--no-interaction' => true])
+            ->expectsConfirmation('Prefix with `Workbench` namespace?', answer: 'yes')
+            ->assertSuccessful();
+
+        $this->assertCommandExecutedWithDevTool();
+        $this->assertCommandExecutedWithoutInstall();
+    }
+
+    #[Test]
+    public function it_can_be_installed_with_prompt_for_missing_arguments()
+    {
+        $this->artisan('workbench:devtool')
+            ->expectsConfirmation('Run Workbench installation?', false)
+            ->expectsConfirmation('Prefix with `Workbench` namespace?', answer: 'yes')
+            ->assertSuccessful();
+
+        $this->assertCommandExecutedWithDevTool();
+        $this->assertCommandExecutedWithoutInstall();
+    }
+}
