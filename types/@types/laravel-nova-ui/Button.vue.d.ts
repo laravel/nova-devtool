1cf1376bc9db04fddab7a30fd147f28ded6240b8
Mior Muhammad Zaki <crynobone@gmail.com>
2025-01-28 22:49:17 +0800

    wip
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- types/@types/laravel-nova-ui/Button.vue.d.ts
+++ types/@types/laravel-nova-ui/Button.vue.d.ts
@@ -59,10 +59,10 @@ declare const _default: import("vue").DefineComponent<import("vue").ExtractPropT
 }>> & Readonly<{}>, {
     size: string;
     variant: string;
-    disabled: boolean;
     as: string;
     state: string;
     padding: string;
     loading: boolean;
+    disabled: boolean;
 }, {}, {}, {}, string, import("vue").ComponentProvideOptions, true, {}, any>;
 export default _default;
