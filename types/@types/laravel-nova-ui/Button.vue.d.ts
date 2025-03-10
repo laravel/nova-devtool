660557b89b199afedd5ae466117b79b2fcaf6c1c
Mior Muhammad Zaki <crynobone@gmail.com>
2025-03-10 22:48:34 +0800

    wip
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- types/@types/laravel-nova-ui/Button.vue.d.ts
+++ types/@types/laravel-nova-ui/Button.vue.d.ts
@@ -58,8 +58,8 @@ declare const _default: import("vue").DefineComponent<import("vue").ExtractPropT
     trailingIcon: {};
 }>> & Readonly<{}>, {
     size: string;
-    variant: string;
     disabled: boolean;
+    variant: string;
     as: string;
     state: string;
     padding: string;
